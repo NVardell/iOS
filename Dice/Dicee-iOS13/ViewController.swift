3630d2044a3e6e84a1966121c1aae89b7a89dc00
NV <nvardell@gmail.com>
2021-10-14 19:45:02 -0400

    Fixed total error on initial load

--- Dice/Dicee-iOS13/ViewController.swift
+++ Dice/Dicee-iOS13/ViewController.swift
@@ -24,10 +24,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceTotal.text = "\(2 + topDie + bottomDie)"
         
         diceIV1.alpha = 0.75
         diceIV2.alpha = 0.75
+        
+        diceIV1.image = dice[topDie]
+        diceIV2.image = dice[bottomDie]
+        diceTotal.text = "\(2 + topDie + bottomDie)"
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
