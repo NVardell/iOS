ef8019eb82a1530426827ec1d17ef0b95b48d758
NV <nvardell@gmail.com>
2021-10-16 17:25:33 -0400

    Removed all extra code from copy/past impl

--- 7/Xylophone/Xylophone/ViewController.swift
+++ 7/Xylophone/Xylophone/ViewController.swift
@@ -24,24 +24,11 @@ class ViewController: UIViewController {
     }
     
     func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else {
-                print("File DNE.")
-                return
-            }
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
-            
-            guard let player = player else { return }
-            
-            player.play()
-            
-        } catch let error {
-            print(error.localizedDescription)
-        }
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
     }
+    
     
 }
 
