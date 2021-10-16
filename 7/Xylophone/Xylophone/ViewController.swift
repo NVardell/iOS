02d8849bbbe4fe640a2a34104b5f050d2fb930b5
NV <nvardell@gmail.com>
2021-10-16 16:42:40 -0400

    Stack Overflow Top Solution

--- 7/Xylophone/Xylophone/ViewController.swift
+++ 7/Xylophone/Xylophone/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,8 +20,28 @@ class ViewController: UIViewController {
 
     @IBAction func cKeyPress(_ sender: UIButton) {
         print("C Key Pressed.")
+        playSound()
     }
     
-
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else {
+                print("File DNE.")
+                return
+            }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
 }
 
