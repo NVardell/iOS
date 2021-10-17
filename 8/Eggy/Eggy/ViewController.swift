4aaa7ccfbeba608ab7b09e593f09cfc3e0a8a41c
NV <nvardell@gmail.com>
2021-10-17 19:21:13 -0400

    Added progress bar and constraints

--- 8/Eggy/Eggy/ViewController.swift
+++ 8/Eggy/Eggy/ViewController.swift
@@ -12,11 +12,40 @@ class ViewController: UIViewController {
 
     let eggTimes = ["Soft":5, "Medium":7, "Hard":12]
     
+    var timer = Timer()
+    var secondsPassed = 0, totalSeconds = 0
+    
     @IBOutlet weak var timerValue: UIView!
-
+    @IBOutlet weak var doneLabel: UILabel!
+    @IBOutlet weak var progBar: UIProgressView!
+    
+    
     @IBAction func hardenessSelected(_ sender: UIButton) {
+        
         let hardness = sender.currentTitle!
-        print(eggTimes[hardness]!)
+        totalSeconds = eggTimes[hardness]!
+        
+        progBar.progress = 0.0
+        secondsPassed = 0
+        doneLabel.text = "Timer started for cooking a \(hardness) egg!"
+
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                             target: self,
+                             selector: #selector(update),
+                             userInfo: nil,
+                             repeats: true)
+    }
+
+    @objc func update() {
+        progBar.progress = Float(secondsPassed) / Float(totalSeconds)
+        if secondsPassed < totalSeconds {
+            secondsPassed += 1
+        } else {
+            timer.invalidate()
+            doneLabel.text = "Done."
+        }
+        
     }
 
 }
