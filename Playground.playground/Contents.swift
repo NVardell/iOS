17b03662bd06be0a84ad9de211695d9c6b70dea3
NV <nvardell@gmail.com>
2021-10-17 14:29:13 -0400

    Love Calc

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,5 +1,15 @@
 
 
 func loveCalculator() {
-    let score = 
+    let score = Int.random(in: 1...100)
+    
+    if score > 80 {
+        print("You love each other like Kanye loves Kanye! Now that's a whole whole lot... ")
+    } else if score > 40 {
+        print("You go together like Coke & Mentos... Yikes!")
+    } else {
+        print("You'll be alone forever & ever.  Sucks to suck!")
+    }
 }
+
+loveCalculator()
