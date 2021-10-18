dd9f9168e72aed2c6e83650d254c792e292e4a1f
NV <nvardell@gmail.com>
2021-10-18 19:06:20 -0400

    Base code for infinite loop of stock Q & A

--- 9/Quiz/Quiz/ViewController.swift
+++ 9/Quiz/Quiz/ViewController.swift
@@ -1,27 +1,82 @@
 //
 //  ViewController.swift
-//  Quizzler-iOS13
+//  Quiz App
 //
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Nate V on 10/17/2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    // Maintaining Current & Completed for future randomization of questions
+    var totalQuestions = 0, completedQuestions = 0, currentQuestion = 0
+    var correct = 0, incorrect = 0
+    
+    // Base Quiz Array for quick testing
+    let quiz = [
+        ["Q1","True"],
+        ["Q2","True"],
+        ["Q3","True"],
+        ["Q4","True"],
+        ["Q5","True"],
+        ["Q6","True"],
+        ["Q7","True"],
+        ["Q8","True"],
+        ["Q9","True"],
+        ["Q10","True"]
+    ]
+    
+    /**
+     * Triggered on initial app load
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        progressBar.progress = 0.0
+        
+        totalQuestions = quiz.count
+        questionLabel.text = quiz[currentQuestion][0]
     }
     
+    
+    /**
+     * Triggered any time an answer button is pressed.
+     */
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let answer = sender.currentTitle
+        let currentAnswer = quiz[currentQuestion][0]
+        
+        if(answer == currentAnswer) {
+            correct+=1
+        } else {
+            incorrect+=1
+        }
+     
+        print("Correct=\(correct) \t Incorrect=\(incorrect)")
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        // Forces an infinite loop of the quiz questions
+        if currentQuestion < totalQuestions-1 {
+            currentQuestion+=1
+        } else {
+            currentQuestion = 0
+            completedQuestions = -1
+        }
+        
+        completedQuestions+=1
+        questionLabel.text = quiz[currentQuestion][0]
+        progressBar.progress = Float(completedQuestions) / Float(totalQuestions)
+    }
 
 }
 
