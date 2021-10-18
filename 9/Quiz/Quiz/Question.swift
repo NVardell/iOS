40e67241771257b646e8b74f9a5712a9fe685414
NV <nvardell@gmail.com>
2021-10-18 19:45:37 -0400

    Created new file for Question structures

---
+++ 9/Quiz/Quiz/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quiz
+//
+//  Created by Nate Vardell on 10/18/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
