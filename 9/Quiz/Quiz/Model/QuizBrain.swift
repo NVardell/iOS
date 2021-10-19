ab8e40c3d8a758673132708125abefd0cc100693
NV <nvardell@gmail.com>
2021-10-18 20:35:16 -0400

    Adding new model class to maintain quiz logic

---
+++ 9/Quiz/Quiz/Model/QuizBrain.swift
@@ -0,0 +1,13 @@
+//
+//  QuizBrain.swift
+//  Quiz
+//
+//  Created by Nate Vardell on 10/18/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct QuizBrain {
+        
+}
