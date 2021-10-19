0fee98b6af5b8eb6b46609a382f7aaf7ae1b391c
NV <nvardell@gmail.com>
2021-10-18 20:22:19 -0400

    Added .2sec timer on bg change to indicate (in)correct answers on input buttons

--- 9/Quiz/Quiz/ViewController.swift
+++ 9/Quiz/Quiz/ViewController.swift
@@ -15,22 +15,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     
     // Maintaining Current & Completed for future randomization of questions
-    var totalQuestions = 0, completedQuestions = 0, currentQuestion = 0
+    var totalQuestions = 0, completedQuestions = 1, currentQuestion = 0
     var correct = 0, incorrect = 0
     
     // Base Quiz Array for quick testing
     let quiz = [
-        Question(text: "Q1", answer: "True"),
-        Question(text: "Q1", answer: "True"),
-        Question(text: "Q2", answer: "True"),
-        Question(text: "Q3", answer: "True"),
-        Question(text: "Q4", answer: "True"),
-        Question(text: "Q5", answer: "True"),
-        Question(text: "Q6", answer: "True"),
-        Question(text: "Q7", answer: "True"),
-        Question(text: "Q8", answer: "True"),
-        Question(text: "Q9", answer: "True"),
-        Question(text: "Q10", answer: "True")
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
     ]
     
     /**
@@ -39,10 +39,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        progressBar.progress = 0.0
-        
         totalQuestions = quiz.count
         questionLabel.text = quiz[currentQuestion].text
+        progressBar.progress = Float(completedQuestions) / Float(totalQuestions)
     }
     
     
@@ -56,15 +55,21 @@ class ViewController: UIViewController {
         
         if(answer == currentAnswer) {
             correct+=1
+            sender.backgroundColor = UIColor.green
         } else {
             incorrect+=1
+            sender.backgroundColor = UIColor.red
         }
      
         print("Correct=\(correct) \t Incorrect=\(incorrect)")
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2,
+                                     target: self,
+                                     selector: #selector(updateUI),
+                                     userInfo: nil,
+                                     repeats: false)
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         
         // Forces an infinite loop of the quiz questions
         if currentQuestion < totalQuestions-1 {
@@ -77,6 +82,8 @@ class ViewController: UIViewController {
         completedQuestions+=1
         questionLabel.text = quiz[currentQuestion].text
         progressBar.progress = Float(completedQuestions) / Float(totalQuestions)
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
 
 }
