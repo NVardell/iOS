8f7481405769e7e52b554d5093e27939e4490982
NV <nvardell@gmail.com>
2021-10-21 17:08:49 -0400

    Created new segue to navigate back to calc view when recalc btn is pressed

--- 11/BMI/BMI/Views/ResultViewController.swift
+++ 11/BMI/BMI/Views/ResultViewController.swift
@@ -10,26 +10,18 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    var bmiValue = ""
+    
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        bmiLabel.text = bmiValue
     }
     
     @IBAction func recalcPressed(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "goToCalculate", sender: self)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
