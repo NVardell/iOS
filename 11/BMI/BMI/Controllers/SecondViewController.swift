7c3347c22d2fe77595831cd2ec115a04683d8c8c
NV <nvardell@gmail.com>
2021-10-21 16:24:47 -0400

    Finished second view to display BMI from landing view 100% programmatically

--- 11/BMI/BMI/Controllers/SecondViewController.swift
+++ 11/BMI/BMI/Controllers/SecondViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var bmiValue = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .red // Red so we can easily see where it's at.
+        
+        // Programatically creating a UI Label
+        let label = UILabel()
+        label.text = "BAM!"
+        // Label - Programatically setting size/position
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
     }
 }
