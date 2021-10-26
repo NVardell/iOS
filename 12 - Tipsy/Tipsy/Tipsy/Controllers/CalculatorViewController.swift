012f70ea134b25164877c26bd8c1d0524d76b920
NV <nvardell@gmail.com>
2021-10-26 19:45:53 -0400

    Implemented remaining functionality

--- 12 - Tipsy/Tipsy/Tipsy/Controllers/CalculatorViewController.swift
+++ 12 - Tipsy/Tipsy/Tipsy/Controllers/CalculatorViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
-    var tipPercent: String?
+    var tip = 0.10
+    var people = 2
+    var billTotal = 0.0
+    var finalTotal = "0.0"
     
     @IBOutlet weak var splitNumberLabel: UILabel!
     @IBOutlet weak var billTextField: UITextField!
@@ -20,21 +23,46 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     
     @IBAction func tipChanged(_ sender: UIButton) {
-        print("Tip Percent Changed.")
         updateSelected()
+        billTextField.endEditing(true)
         sender.isSelected = true
         
-        tipPercent = sender.currentTitle ?? "0"
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleValue = String(buttonTitle.dropLast())
+        let buttonTitleValueAsNum = Double(buttonTitleValue)!
+        tip = buttonTitleValueAsNum / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        people = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print("Tip percent = \(tipPercent ?? "0")")
+        let bill = billTextField.text!
+        print("Bill Total = \(bill)")
+        
+        if bill != "" {
+            billTotal = Double(bill)!
+            let result = billTotal * (1+tip) / Double(people)
+            finalTotal = String(format: "%.2f", result)
+            print("Final Total = \(finalTotal)")
+        }
+        
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResults" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.result = finalTotal
+            destinationVC.tip = Int(tip*100)
+            destinationVC.split = people
+        }
     }
     
     func updateSelected() {
+        print("Tip Percent Changed.")
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
