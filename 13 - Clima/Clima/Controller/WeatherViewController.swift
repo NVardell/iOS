e509f6894f247a4084d96c7c7456faac5dadd52a
NV <nvardell@gmail.com>
2021-10-30 01:45:37 -0400

    Implemented current location using long/lat in addition to city search

--- 13 - Clima/Clima/Controller/WeatherViewController.swift
+++ 13 - Clima/Clima/Controller/WeatherViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
 
-    var manager = WeatherManager()
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -20,14 +22,26 @@ class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        cityLabel.text = ""
+        temperatureLabel.text = "0.0"
+        // Request Location
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        // Listener - Search Text Field
         searchTextField.delegate = self
-        manager.delegate = self
+        // Listener - Weather Manager
+        weatherManager.delegate = self
     }
     
 
     @IBAction func searchPressed(_ sender: UIButton) {
         searchTextField.endEditing(true)
         print(searchTextField.text!)
+    }
+    @IBAction func currentLocationPressed(_ sender: UIButton) {
+        print("Requesting current location...")
+        locationManager.requestLocation()
     }
     
 }
@@ -52,7 +66,7 @@ extension WeatherViewController: UITextFieldDelegate {
         
         // Validate user input
         if let city = searchTextField.text {
-            manager.fetchWeather(cityName: city)
+            weatherManager.fetchWeather(cityName: city)
             cityLabel.text = city
         }
         
@@ -65,13 +79,32 @@ extension WeatherViewController: UITextFieldDelegate {
 // MARK: - WeatherManagerDelegate
 extension WeatherViewController: WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
-        print(weather) // Kalamazoo
+        print(weather) // Kalamazoo "lat": 42.2917, "lon": -85.5872
         DispatchQueue.main.async {
+            self.cityLabel.text = weather.cityName
             self.temperatureLabel.text = weather.temperatureString
             self.conditionImageView.image = UIImage(systemName: weather.conditionName)
         }
     }
     func didFailWithError(_ weatherManager: WeatherManager, error: Error) {
+        print(error)
+    }
+}
+
+
+// MARK: - CLLocationManagerDelegate
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Got Location Data")
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()  // Grab current location & stop future attempts.
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            print("Lat: \(lat)\tLon:\(lon)")
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
 }
