d2c2a1773652d672cb3519c0902e15c8a14873da
NV <nvardell@gmail.com>
2021-11-03 18:08:01 -0400

    Setup test messages array for table view display

--- Other/Projects/FlashyChat/FlashyChat/Controllers/ChatViewController.swift
+++ Other/Projects/FlashyChat/FlashyChat/Controllers/ChatViewController.swift
@@ -6,19 +6,57 @@
 //
 
 import UIKit
+import Firebase
 
 class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "Aziz", body: "Blah, blah, blah!"),
+        Message(sender: "Michael", body: "Read receipt."),
+        Message(sender: "Aziz", body: "I see, it say, you saw! Blah, blah, blah!"),
+        Message(sender: "Michael", body: "Read receipt.")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        navigationItem.hidesBackButton = true
+        title = K.appName
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
+}
 
+
+
+// MARK: - UITableViewDataSource
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(messages.count)
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Table View ~ Cell for Row At: \(indexPath.row)")
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        let sender = messages[indexPath.row].sender
+        if sender != "Aziz" {
+            cell.textLabel?.textAlignment = NSTextAlignment.right
+        }
+        return cell
+    }
 }
