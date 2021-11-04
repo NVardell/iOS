88ffa50e04f6b7c8358382685148f718b7560af4
NV <nvardell@gmail.com>
2021-11-04 12:55:18 -0400

    Created Message Cell class & design xib file

---
+++ Other/Projects/FlashyChat/FlashyChat/Views/MessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageCell.swift
+//  FlashyChat
+//
+//  Created by Nate Vardell on 11/3/21.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
