6daf719d55c9995c757526f6685305ab79ced327
NV <nvardell@gmail.com>
2021-11-05 14:23:05 -0400

    Cast Prototype TVCell to new custom xib

--- Other/Projects/FlashyChat/FlashyChat/Controllers/ChatViewController.swift
+++ Other/Projects/FlashyChat/FlashyChat/Controllers/ChatViewController.swift
@@ -16,13 +16,14 @@ class ChatViewController: UIViewController {
     var messages: [Message] = [
         Message(sender: "Aziz", body: "Blah, blah, blah!"),
         Message(sender: "Michael", body: "Read receipt."),
-        Message(sender: "Aziz", body: "I see, it say, you saw! Blah, blah, blah!"),
+        Message(sender: "Aziz", body: "I see, it say, you saw! Blah, blah, blah! Better see more than a 'Read 1 second ago.' in a minute here!!! Gr."),
         Message(sender: "Michael", body: "Read receipt.")
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
         navigationItem.hidesBackButton = true
         title = K.appName
     }
@@ -51,12 +52,27 @@ extension ChatViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         print("Table View ~ Cell for Row At: \(indexPath.row)")
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
-        let sender = messages[indexPath.row].sender
-        if sender != "Aziz" {
-            cell.textLabel?.textAlignment = NSTextAlignment.right
+        return horizontalFlipTableViewRow(cellForRowAt: indexPath)
+    }
+    
+    /**
+     * Quick & dirty solution to making chat messages appear on opposing sides dependant on sender.
+     *
+     * - Parameter indexPath: Row index of table view
+     * - Returns: MessageCell for displaying in table view
+     */
+    func horizontalFlipTableViewRow(cellForRowAt indexPath: IndexPath) -> MessageCell {
+        let message = messages[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        if message.sender != "Aziz" {
+            cell.transform = CGAffineTransform(scaleX: -1, y: 1)
+            cell.messageBubble.transform = CGAffineTransform(scaleX: -1, y: 1)
+            cell.rightImageView.transform = CGAffineTransform(scaleX: -1, y: 1)
         }
+        
+        cell.messageLabel.text = message.body
+        
         return cell
     }
 }
