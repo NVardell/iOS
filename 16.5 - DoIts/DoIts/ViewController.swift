69ccf30e9f54fabf509f813352f22b9b24ecc83c
NV <nvardell@gmail.com>
2021-11-14 16:54:31 -0500

    Pushing empty project for ToDo list app

---
+++ 16.5 - DoIts/DoIts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoIts
+//
+//  Created by Nate Vardell on 11/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
