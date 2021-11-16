5ece6509f3863b7f738a43dc6aa3f0d31c091ab8
NV <nvardell@gmail.com>
2021-11-15 22:59:38 -0500

    Moved data persistence from UserDefault to custom Items.plist file

--- 16.5 - DoIts/DoIts/Controller/ToDoListViewController.swift
+++ 16.5 - DoIts/DoIts/Controller/ToDoListViewController.swift
@@ -9,18 +9,15 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    // var netflixBest = ["Stranger Things", "The Witcher", "Lost in Space"]
     var netflixBest = [Item]()
-    
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        netflixBest.append(Item(t: "Stranger Things"))
-        netflixBest.append(Item(t: "The Witcher"))
-        netflixBest.append(Item(t: "Lost in Space"))
-        // if let items = defaults.array(forKey: "ToDoListArray") as? [String] { netflixBest = items }
+        print("Data File Path = \(dataFilePath!)")
+        
+        loadItems()
     }
 
     
@@ -29,7 +26,6 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return netflixBest.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Setup object being used in cell
@@ -50,12 +46,17 @@ class ToDoListViewController: UITableViewController {
     
     // MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Selected Row At = \(indexPath.row)")
-        tableView.deselectRow(at: indexPath, animated: true)
         
         // Swithes the done state of an item for alternating checked/unchecked on UI
         netflixBest[indexPath.row].done = !netflixBest[indexPath.row].done
+        
+        // Save updated checkmarks
+        saveItems()
+        
+        // Prevent row from staying selected/shaded
+        tableView.deselectRow(at: indexPath, animated: true)
     }
+    
     
     
     // MARK: - Add New Items
@@ -67,18 +68,11 @@ class ToDoListViewController: UITableViewController {
         // Create user interaction piece of the UIAlert being displayed to user
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
-            // Setup new item to add to list
-            // let newItem = Item(t: textField.text!)
-            
             // What happens when the user clicks the Add Item button on our UIAlert.
             self.netflixBest.append(Item(t: textField.text!))
-            print("\nSuccessfully added item: \(String(describing: textField.text))")
             
-            // Add updated list to User Defaults database to persist new ToDo list items
-            self.defaults.set(self.netflixBest, forKey: "ToDoListArray")
-            
-            // Reload the TableView so the newly added item will appear in view
-            self.tableView.reloadData()
+            // Save updated list to persist newly added items
+            self.saveItems()
         }
         
         // Add newly created UIAlertAction to our Alert we want to display
@@ -93,6 +87,29 @@ class ToDoListViewController: UITableViewController {
         
         // Display newly created alert
         present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func saveItems() {
+        do {
+            let data = try PropertyListEncoder().encode(self.netflixBest)
+            try data.write(to: self.dataFilePath!)
+            
+            // Reload the TableView so the updated item will appear in TableView
+            tableView.reloadData()
+        } catch {
+            print("Error encoding array. Error: \(error)")
+        }
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            do {
+                netflixBest = try PropertyListDecoder().decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array. Error: \(error)")
+            }
+        }
     }
 }
 
