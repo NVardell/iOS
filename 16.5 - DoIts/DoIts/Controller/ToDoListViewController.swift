e51a4e79ac82b9277ba01435a403aa43f5206726
NV <nvardell@gmail.com>
2021-11-19 16:26:59 -0500

    Implemented List Item Parent Categories

--- 16.5 - DoIts/DoIts/Controller/ToDoListViewController.swift
+++ 16.5 - DoIts/DoIts/Controller/ToDoListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ToDoListViewController.swift
 //  DoIts
 //
 //  Created by Nate Vardell on 11/14/21.
@@ -11,14 +11,16 @@ import CoreData
 class ToDoListViewController: UITableViewController {
 
     var itemArray = [Item]()
+    var selectedCat : Category? {
+        didSet {
+            loadItems()
+        }
+    }
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         print("\nData File Path = \(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))\n")
-
-        loadItems()
     }
 
     // MARK: - Add New Items
@@ -31,11 +33,12 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (_) in
 
             // What happens when the user clicks the Add Item button on our UIAlert.
-            // self.netflixBest.append(Item(t: textField.text!))
+            // self.itemArray.append(Item(t: textField.text!))
 
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCat
 
             // Save updated list to persist newly added items
             self.saveItems()
@@ -62,7 +65,13 @@ class ToDoListViewController: UITableViewController {
         loadItems()
     }
 
-    func loadItems(with req: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with req: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        // Create predicate requiring search results to have current category as parent
+        let catPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCat!.name!)
+        if let searchPredicate = predicate { req.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [catPredicate, searchPredicate]) }
+        else { req.predicate = catPredicate}
+        
         // Attempt to fetch request for TableView Data
         do { itemArray = try context.fetch(req) } catch { print("Error fetching date.  Error: \(error)") }
 
@@ -80,10 +89,10 @@ class ToDoListViewController: UITableViewController {
         let item = itemArray[indexPath.row]
 
         // Setup row cell for display in TableView
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.Cells.toDoItems, for: indexPath)
 
         // Set cell attributes based on the attribute values of our item
-        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.textLabel?.text = item.title
         cell.accessoryType = item.done ? .checkmark : .none
 
         // Return cell for display in TableView
@@ -118,10 +127,9 @@ extension ToDoListViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("Searching for: \(searchBar.text!)")
         let req: NSFetchRequest<Item> = Item.fetchRequest()
-        req.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         req.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
 
-        loadItems(with: req)
+        loadItems(with: req, predicate: NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!))
     }
 
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
