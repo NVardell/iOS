b3d52900109037f61ee00384a10600fd8242ac41
NV <nvardell@gmail.com>
2021-12-02 16:05:39 -0500

    Replaced the stock project files with the base Calc Project files.

--- 17_Calc/Calc/Calc/ViewController.swift
+++ 17_Calc/Calc/Calc/ViewController.swift
@@ -1,19 +1,31 @@
 //
 //  ViewController.swift
-//  Calc
+//  Calculator
 //
-//  Created by Nate Vardell on 12/2/21.
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+    
     }
 
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+    
+    }
 
 }
 
