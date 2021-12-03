06b4528dd36a1cf5f265962e29097d9a0e197798
NV <nvardell@gmail.com>
2021-12-03 15:24:24 -0500

    WIP | Impl for nums

--- 17_Calc/Calc/Calc/Controller/CalcViewController.swift
+++ 17_Calc/Calc/Calc/Controller/CalcViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class CalcViewController: UIViewController {
     
+    var currentTotal = "0"
+    var finishedTyping = true
     @IBOutlet weak var displayLabel: UILabel!
     
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
+        // Reset Finished Typing
+        finishedTyping = true
+        
+        
         //What should happen when a non-number button is pressed
-    
+        if let operation = sender.currentTitle {
+            switch operation {
+                case "AC":
+                    currentTotal = "0"
+                    displayLabel.text = currentTotal
+                default:
+                    displayLabel.text = "0"
+            }
+        }
+        
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+
+            if finishedTyping && numValue != "0" {
+                displayLabel.text = numValue
+                finishedTyping = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
     
+    func updateTotalLabel() {
+        print("Updating Total.")
     }
 
 }
