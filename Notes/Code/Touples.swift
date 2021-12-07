c98ccd3717b5bf231fb32087c86ff6282678c02f
NV <nvardell@gmail.com>
2021-12-07 11:17:47 -0500

    Added code snippets for Touples

---
+++ Notes/Code/Touples.swift
@@ -0,0 +1,15 @@
+// 
+// TOUPLES
+// ########################################################
+// Example #1 - Simplest Touple
+let t1 = ("Nate", 30)
+print(t1.0)
+
+// Example #2 - Simpler Touple
+let t2 = (name:"Nate", age:30)
+print(t2.name)
+
+// Example #3 - Simple Touple
+let t3 = (name:String, age:Int)
+t3.name = ("Nate", 30)
+print(t3.name)
