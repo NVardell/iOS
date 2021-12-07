a9c5893bc798c3780819336a9184378d7d473a25
NV <nvardell@gmail.com>
2021-12-07 17:18:19 -0500

    Example impl for do, try, catch

--- 00_InterviewTopics/DoTryCatch/DoTryCatch/ViewController.swift
+++ 00_InterviewTopics/DoTryCatch/DoTryCatch/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    enum LoginError: Error {
+        case incompleteForm
+        case invalidEmail
+        case incorrectPasswordLength
+    }
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        do {
+            let imageData = try Data(contentsOf: URL(fileURLWithPath: "filePathHere"))
+            // Do whatever you want with ImageData because the try succeeded
+        } catch {
+            // Handle the error
+            print(error.localizedDescription)
+        }
+    }
 }
 
