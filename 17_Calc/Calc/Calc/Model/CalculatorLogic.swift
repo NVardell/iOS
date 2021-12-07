e60aad81979a15ca86e24b0a24337c492c29a98b
NV <nvardell@gmail.com>
2021-12-07 11:17:25 -0500

    Extracted operation functions to designated function

--- 17_Calc/Calc/Calc/Model/CalculatorLogic.swift
+++ 17_Calc/Calc/Calc/Model/CalculatorLogic.swift
@@ -9,41 +9,61 @@ import Foundation
 
 struct CalculatorLogic {
     
-    var number: Double
-    init(number: Double) {
+    private var number: Double?
+    private var intermediateCalculation: (n1:Double, operation:String)?
+    
+    mutating func setNum(_ number: Double) {
+        print("Setting Calculator Number = \(number)")
         self.number = number
     }
     
-    func operationOperations(computation operation: String) -> Double {
-            print("Operation is: \(operation)")
-            switch operation {
+    mutating func operationOperations(computation symbol: String) -> Double? {
+        print("Operation is: \(symbol)")
+        
+        if let n = number {
+            switch symbol {
                 case "AC":
                     print("\tCase: AC")
                     return 0.0
                 case "+/-":
                     print("\tCase: +/-")
-                    return number * -1
+                    return n * -1
                 case "%":
                     print("\tCase: %")
-                    return number * 0.01
-                case "÷":
-                    print("\tCase: ÷")
-                    return number
-                case "×":
-                    print("\tCase: ×")
-                    return number
-                case "-":
-                    print("\tCase: -")
-                    return number
-                case "+":
-                    print("\tCase: +")
-                    return number
+                    return n * 0.01
                 case "=":
                     print("\tCase: =")
-                    return number
+                    return performCalculation(n2: n)
                 default:
                     print("\tCase: Default")
-                    return 0.0
+                    return nil
             }
+        } else {
+            return nil
+        }
+    }
+    
+    mutating private func performCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.n1,
+            let operation = intermediateCalculation?.operation {
+            
+            switch operation {
+                case "+":
+                    print("\t\tCase: +")
+                    return n1 + n2
+                case "-":
+                    print("\t\tCase: -")
+                    return n1 - n2
+                case "×":
+                    print("\t\tCase: ×")
+                    return n1 * n2
+                case "÷":
+                    print("\t\tCase: ÷")
+                    return n1 / n2
+                default:
+                    fatalError("")
+            }
+        }
+        return 0.0
     }
 }
