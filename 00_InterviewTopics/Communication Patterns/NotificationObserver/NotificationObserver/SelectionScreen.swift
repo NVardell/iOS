8c39c67218bf8a265d699d8eb9128ecad1ea0dbb
NV <nvardell@gmail.com>
2021-12-13 15:30:54 -0500

    Working Notification/Observer Project

---
+++ 00_InterviewTopics/Communication Patterns/NotificationObserver/NotificationObserver/SelectionScreen.swift
@@ -0,0 +1,27 @@
+//
+//  SelectionScreen.swift
+//  NotificationObserver
+//
+//  Created by Nate Vardell on 12/13/21.
+//
+
+import UIKit
+
+class SelectionScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func imperialButtonTapped(_ sender: UIButton) {
+        let name = Notification.Name(rawValue: darkNotificationKey)
+        NotificationCenter.default.post(name: name, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func rebelButtonTapped(_ sender: UIButton) {
+        let name = Notification.Name(rawValue: lightNotificationKey)
+        NotificationCenter.default.post(name: name, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+}
