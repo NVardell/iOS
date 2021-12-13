68fa36ad4eaa54a333ade28f1cf757fe225dafec
NV <nvardell@gmail.com>
2021-12-13 16:49:49 -0500

    Setup function for adding multiple observers for each of our 2 notifications

--- 00_InterviewTopics/Communication Patterns/NotificationObserver/NotificationObserver/BaseScreen.swift
+++ 00_InterviewTopics/Communication Patterns/NotificationObserver/NotificationObserver/BaseScreen.swift
@@ -33,4 +33,17 @@ class BaseScreen: UIViewController {
         present(selectionVC, animated: true, completion: nil)
     }
     
+    
+    /// Create our observers to wait on notifications triggered by our Selection Screen
+    func createObservers() {
+        // Light Side
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateCharacterImage(notification:)), name: light, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateCharacterName(notification:)), name: light, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateBackground(notification:)), name: light, object: nil)
+        
+        // Dark Side
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateCharacterImage(notification:)), name: dark, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateCharacterName(notification:)), name: dark, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(BaseScreen.updateBackground(notification:)), name: dark, object: nil)
+    }
 }
