8d00a1922f13535943a6ae20f8a5499d8058c4b6
NV <nvardell@gmail.com>
2021-12-15 13:40:14 -0500

    Implemented base pan gesture functionality

--- 00_InterviewTopics/Gesture Rocognizers/PanGesture/PanGesture/ViewController.swift
+++ 00_InterviewTopics/Gesture Rocognizers/PanGesture/PanGesture/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fileImageView: UIImageView!
+    @IBOutlet weak var trashImageView: UIImageView!
+    
+    var fileViewOrigin: CGPoint!  // Icon frame reference for reset
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Add Pan Gesture to our File Image
+        addPanGesture(view: fileImageView)
+        
+        // Set location of File Icon on screen
+        fileViewOrigin = fileImageView.frame.origin
+        
+        // Forces File Icon to appear above trash icon
+        view.bringSubviewToFront(fileImageView)
     }
 
+    
+    /// Gesture | Add Pan Gesture to view
+    func addPanGesture(view: UIView) {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePan(sender:)))
+        view.addGestureRecognizer(pan)
+    }
 
+    
+    /// Gesture | Handle Image Pan Gesture
+    @objc func handlePan(sender: UIPanGestureRecognizer) {
+        let fileView = sender.view!  // Sender is our pan gesture that is attached to our file image
+        let trans = sender.translation(in: view)
+        
+        switch sender.state {
+            case .began, .changed:
+                fileView.center = CGPoint(
+                    x: fileView.center.x + trans.x,
+                    y: fileView.center.y + trans.y)
+                
+                sender.setTranslation(CGPoint.zero, in: view)
+            case .ended:
+                print("Ended Case")
+                // 1. Check if File Icon Overlaps our Trash Icon
+                if fileView.frame.intersects(trashImageView.frame) {
+                    // 1a. Remove File Icon from View
+                    UIView.animate(withDuration: 0.3) {
+                        self.fileImageView.alpha = 0.0
+                    }
+                } else {
+                    // 1b. Move File Icon back to Origin
+                    UIView.animate(withDuration: 0.3) {
+                        self.fileImageView.frame.origin = self.fileViewOrigin
+                    }
+                }
+            default:
+                print("Default Case")
+        }
+    }
 }
 
