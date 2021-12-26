d0f123a291d491ebccbb58e913dd7c56e7e496af
NV <nvardell@gmail.com>
2021-12-26 14:30:51 -0500

    Pushing base Hot Diggity ML app

---
+++ 19_HotDiggity/HotDiggity/HotDiggity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotDiggity
+//
+//  Created by Nate Vardell on 12/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
